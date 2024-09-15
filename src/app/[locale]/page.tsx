9b4edbf72be645e45c81893669fff28f6de71d35
olebausp <ole.bresler@t-online.de>
2024-09-15 18:50:20 +0200

    example UI

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,33 @@
+import Constraint from "@/components/Constraint/Constraint";
+import Variable from "@/components/Variable/Variable";
+import { Box, Card, Container, Grid2, MenuItem, Select, TextField } from "@mui/material";
+import { useTranslations } from "next-intl";
+import React from "react";
+
 export default function Home() {
-  console.log("Hello World");
+  const t = useTranslations();
 
   return (
-    <div>
-      <p>Hello World</p>
-    </div>
+    <Container maxWidth="lg">
+      <Grid2 container rowSpacing={3} columnSpacing={{xs: 1, sm: 2, md: 3 }}>
+        <Card variant="outlined">
+          <Variable/>
+          <Variable/>
+        </Card>
+        <Card variant="outlined">
+          <Grid2 container rowSpacing={3} columnSpacing={{xs: 1, sm: 2, md: 3 }}>
+            <Select labelId="demo-simple-select-standard-label" id="function_type" label={t("input.function_type")} autoWidth defaultValue={0}>
+              <MenuItem value={0}>{t("input.function_type.min")}</MenuItem>
+              <MenuItem value={1}>{t("input.function_type.max")}</MenuItem>
+            </Select>
+            <TextField id="function" label={t("input.function")}/>
+          </Grid2>
+        </Card>
+        <Card variant="outlined">
+          <Constraint/>
+          <Constraint/>
+        </Card>
+      </Grid2>
+    </Container>
   );
 }
