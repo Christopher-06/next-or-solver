7f15fda128ae537b7e637f4ae5fdaaf1b52de0e4
Christopher-06 <christopher-kock@outlook.de>
2024-09-17 15:03:23 +0200

    Preventing SSR dark mode flickering

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,6 @@
 import { NextIntlClientProvider, useMessages, useLocale } from "next-intl";
 import CssBaseline from "@mui/material/CssBaseline";
+import InitColorSchemeScript from "@mui/material/InitColorSchemeScript";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -18,25 +19,29 @@ export default function RootLayout({
   const locale = useLocale();
 
   return (
-    <html lang={locale}>
-      <NextIntlClientProvider messages={messages}>
-        <Theme>
-          <CssBaseline />
-          <body>
-            <header>
-              {/* MUI Mobile First */}
-              <meta
-                name="viewport"
-                content="initial-scale=1, width=device-width"
-              />
-            </header>
+    <html lang={locale} suppressHydrationWarning>
+      <body>
+        <NextIntlClientProvider messages={messages}>
+          <InitColorSchemeScript attribute="class" />
 
-            <NavBar />
+          <main>
+            <Theme>
+              <CssBaseline />
+              <header>
+                {/* MUI Mobile First */}
+                <meta
+                  name="viewport"
+                  content="initial-scale=1, width=device-width"
+                />
+              </header>
 
-            {children}
-          </body>
-        </Theme>
-      </NextIntlClientProvider>
+              <NavBar />
+
+              {children}
+            </Theme>
+          </main>
+        </NextIntlClientProvider>
+      </body>
     </html>
   );
 }
