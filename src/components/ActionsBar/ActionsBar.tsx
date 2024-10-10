24d35a3c46b1d193fd23de425ec118bf09b0f90a
Christopher-06 <christopher-kock@outlook.de>
2024-10-10 22:18:12 +0200

    Add 1000px minWidth

--- src/components/ActionsBar/ActionsBar.tsx
+++ src/components/ActionsBar/ActionsBar.tsx
@@ -57,24 +57,22 @@ export default function ActionsBar() {
         })
       );
     }
-  }
+  };
 
   const handleSolveClick = async () => {
     dispatch(startSolving(inputType));
 
-    // inject conversion from EASY UI to GMPL
-    if (inputType == "EASY_UI") {
-      console.log("Converting EASY UI to GMPL");
-      textFieldValue = ConvertToGMPL(easyUiModell, easyUiVariables);
-      currentFormat = FileFormat.GMPL;
-    }
+    try {
+      // inject conversion from EASY UI to GMPL
+      if (inputType == "EASY_UI") {
+        console.log("Converting EASY UI to GMPL");
+        textFieldValue = ConvertToGMPL(easyUiModell, easyUiVariables);
+        currentFormat = FileFormat.GMPL;
+      }
 
       if (selectedSolver === "HIGHS") {
         // Run HIGHs solver
-        const solution = await solveHIGHS(
-          textFieldValue,
-          currentFormat
-        );
+        const solution = await solveHIGHS(textFieldValue, currentFormat);
         dispatch(setSolution({ key: inputType, solution }));
       } else if (selectedSolver === "GLPK") {
         // Run GLPK solver
@@ -98,7 +96,12 @@ export default function ActionsBar() {
           error: new Error("Solver nicht gefunden"),
         });
       }
-  }
+    } catch (errorUnknown) {
+      console.log("Error while solving", errorUnknown);
+      const error: Error = errorUnknown as Error;
+      dispatch(setSolutionError({ key: inputType, error }));
+    }
+  };
 
   return (
     <>
@@ -122,12 +125,19 @@ export default function ActionsBar() {
             />
           </Stack>
         </Grid2>
-        <Grid2 size={{ xs: 12, sm: 6 }}>
-          <Stack direction="row" spacing={2} justifyContent="end">
+        <Grid2 size={{ xs: 12, sm: 6 }}
+          sx={{  justifyContent : "center" }}
+        >
+          <Stack
+            direction="row"
+            spacing={2}
+            justifyContent="end"
+            alignItems="center"
+          >
             <Button
               variant="contained"
               color="warning"
-              onClick={handleDeleteAllClick }
+              onClick={handleDeleteAllClick}
             >
               {t("actions_bar.actions_bar.btn_delete_all")}
             </Button>
@@ -137,21 +147,18 @@ export default function ActionsBar() {
               onClick={handleSolveClick}
             >
               {t("actions_bar.actions_bar.btn_solve")}
-              </Button>
+            </Button>
 
-
-              <Select
-                value={selectedSolver}
-                onChange={(e) => setSelectedSolver(e.target.value)}
-                sx={{  ml : 3, }}
-                // variant="standard"
-              >
-                <MenuItem color="primary" value="HIGHS">
-                  HIGHS Solver
-                </MenuItem>
-                <MenuItem value="GLPK">GLPK Solver</MenuItem>
-              </Select>
-
+            <Select
+              value={selectedSolver}
+              onChange={(e) => setSelectedSolver(e.target.value)}
+              sx={{ ml: 3 }}
+            >
+              <MenuItem color="primary" value="HIGHS">
+                HIGHS Solver
+              </MenuItem>
+              <MenuItem value="GLPK">GLPK Solver</MenuItem>
+            </Select>
           </Stack>
         </Grid2>
       </Grid2>
