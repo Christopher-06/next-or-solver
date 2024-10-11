971b2b5e08074c53de235b3bac757682524f6085
Christopher-06 <christopher-kock@outlook.de>
2024-10-11 10:30:06 +0200

    Export Easy UI to GMPL or LP

--- src/components/ActionsBar/ActionsBar.tsx
+++ src/components/ActionsBar/ActionsBar.tsx
@@ -22,6 +22,8 @@ import { InputType } from "@/store/slices/InputType";
 import { setTextFieldValue } from "@/store/slices/TextFieldInputs";
 import { clearAllModell } from "@/store/slices/Modell";
 import ConvertToGMPL from "@/lib/easy-ui/converter";
+import { Modell } from "@/lib/types/Modell";
+import { Variable } from "@/lib/types/Variable";
 
 const FILEFORMAT_MAP: { [key in InputType]: FileFormat } = {
   GMPL: FileFormat.GMPL,
@@ -40,8 +42,10 @@ export default function ActionsBar() {
   let textFieldValue = textFieldInputs[inputType].textFieldValue;
   let currentFormat = FILEFORMAT_MAP[inputType];
   const [selectedSolver, setSelectedSolver] = React.useState("HIGHS");
-  const easyUiModell = useSelector((state: RootState) => state.modell);
-  const easyUiVariables = useSelector((state: RootState) => state.variables);
+  const easyUiModell: Modell = useSelector((state: RootState) => state.modell);
+  const easyUiVariables: Variable[] = useSelector(
+    (state: RootState) => state.variables
+  );
 
   const handleDeleteAllClick = () => {
     dispatch(clearSolution(inputType));
@@ -110,24 +114,28 @@ export default function ActionsBar() {
           <Stack direction="row" spacing={2} justifyContent="start">
             <ExportButton
               content={textFieldValue}
+              easyUiModell={easyUiModell}
+              easyUiVariables={easyUiVariables}
               currentFormat={currentFormat}
               targetFormat={FileFormat.GMPL}
             />
             <ExportButton
               content={textFieldValue}
+              easyUiModell={easyUiModell}
+              easyUiVariables={easyUiVariables}
               currentFormat={currentFormat}
               targetFormat={FileFormat.CPLEX_LP}
             />
             <ExportButton
               content={textFieldValue}
+              easyUiModell={easyUiModell}
+              easyUiVariables={easyUiVariables}
               currentFormat={currentFormat}
               targetFormat={FileFormat.MPS}
             />
           </Stack>
         </Grid2>
-        <Grid2 size={{ xs: 12, sm: 6 }}
-          sx={{  justifyContent : "center" }}
-        >
+        <Grid2 size={{ xs: 12, sm: 6 }} sx={{ justifyContent: "center" }}>
           <Stack
             direction="row"
             spacing={2}
--- src/components/Editor/ExportButton.tsx
+++ src/components/Editor/ExportButton.tsx
@@ -1,61 +1,87 @@
 "use client";
 
-import { Button } from '@mui/material';
-import { FileFormat } from '../Converter/FileFormat';
-import convertLP from '../Converter/Converter';
-import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import { Button } from "@mui/material";
+import { FileFormat } from "../Converter/FileFormat";
+import convertLP from "../Converter/Converter";
+import FileDownloadIcon from "@mui/icons-material/FileDownload";
+import { Modell } from "@/lib/types/Modell";
+import { Variable } from "@/lib/types/Variable";
+import ConvertToGMPL from "@/lib/easy-ui/converter";
 
 interface ExportButtonProps {
-    content: string;
-    currentFormat: FileFormat;
-    targetFormat: FileFormat; // Enum für das Ziel-Format
+  content: string;
+  currentFormat: FileFormat;
+  targetFormat: FileFormat; // Enum für das Ziel-Format
+  easyUiModell?: Modell;
+  easyUiVariables?: Variable[];
 }
 
-const ExportButton: React.FC<ExportButtonProps> = ({ content, currentFormat, targetFormat }) => {
+const ExportButton: React.FC<ExportButtonProps> = ({
+  content,
+  currentFormat,
+  targetFormat,
+  easyUiModell = null,
+  easyUiVariables = null,
+}) => {
+  // Bestimme das Format basierend auf der Dateiendung
+  const getFileExtension = (format: FileFormat): string | null => {
+    if (format === FileFormat.GMPL) return ".mod";
+    if (format === FileFormat.CPLEX_LP) return ".lp";
+    if (format === FileFormat.MPS) return ".mps";
+    return null;
+  };
 
-    // Bestimme das Format basierend auf der Dateiendung
-    const getFileExtension = (format: FileFormat): string | null => {
-        if (format === FileFormat.GMPL) return '.mod';
-        if (format === FileFormat.CPLEX_LP) return '.lp';
-        if (format === FileFormat.MPS) return '.mps';   
-        return null;
-    };
+  // inject conversion from EASY UI to GMPL
+  if (currentFormat == FileFormat.EASY_UI && easyUiModell && easyUiVariables) {
+    console.log("Converting EASY UI to GMPL");
+    content = ConvertToGMPL(easyUiModell, easyUiVariables);
+    currentFormat = FileFormat.GMPL;
+  }
 
-    const saveFile = () => {
-        const convertedContent = convertLP(content || '', currentFormat, targetFormat); // Konvertierter Inhalt
-        const fileExtension = getFileExtension(targetFormat) || '';
-    
-        const blob = new Blob([convertedContent || ""], { type: 'text/plain' });
-        const url = URL.createObjectURL(blob);
-    
-        const link = document.createElement('a');
-        link.href = url;
-        link.download = `problem${fileExtension}`;
-        link.click();
-        
-        URL.revokeObjectURL(url);
-    };
+  const saveFile = () => {
+    const convertedContent = convertLP(
+      content || "",
+      currentFormat,
+      targetFormat
+    ); // Konvertierter Inhalt
+    const fileExtension = getFileExtension(targetFormat) || "";
 
-    // Handle File Save
-    const handleSaveFile = () => {
-        if (content) {
-            saveFile(); // Datei speichern
-        }
-    };
+    const blob = new Blob([convertedContent || ""], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
 
-    return (
-        < >
-            {/* Datei speichern */}
-            <Button 
-                variant="contained" 
-                onClick={handleSaveFile} 
-                style={{ marginTop: '10px', float: 'right', marginRight: 3, marginLeft: 3 }}
-            >
-                <FileDownloadIcon sx={{ mr: 1 }} />
-                Export {targetFormat}
-            </Button>
-        </ >
-    );
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = `problem${fileExtension}`;
+    link.click();
+
+    URL.revokeObjectURL(url);
+  };
+
+  // Handle File Save
+  const handleSaveFile = () => {
+    if (content) {
+      saveFile(); // Datei speichern
+    }
+  };
+
+  return (
+    <>
+      {/* Datei speichern */}
+      <Button
+        variant="contained"
+        onClick={handleSaveFile}
+        style={{
+          marginTop: "10px",
+          float: "right",
+          marginRight: 3,
+          marginLeft: 3,
+        }}
+      >
+        <FileDownloadIcon sx={{ mr: 1 }} />
+        Export {targetFormat}
+      </Button>
+    </>
+  );
 };
 
 export default ExportButton;
